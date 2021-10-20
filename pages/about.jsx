e773de32b8208ede08144e4077e7b958bcc02334
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2021-10-20 13:36:21 -0300

    First commit

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function about() {
+    return (
+        <Layout 
+            title="About"
+            description="Agrege una descripcion"
+        >
+            <h1>About</h1>
+        </Layout>
+    )
+}
